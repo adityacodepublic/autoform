3379926533db437f85c1fb5aa8c67c8ea9fdf2a8
gitcode <website0wordpress@gmail.com>
2025-02-22 15:56:05 +0530

    fix: make mui select boolean field controlled

--- packages/mui/src/components/BooleanField.tsx
+++ packages/mui/src/components/BooleanField.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import Checkbox from "@mui/material/Checkbox";
+import { useController } from "react-hook-form";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import { AutoFormFieldProps } from "@autoform/react";
 
 export const BooleanField: React.FC<AutoFormFieldProps> = ({
+  id,
   label,
   inputProps,
 }) => {
-  const { key, ...props } = inputProps;
+  const { key, onChange, onBlur, ref, ...props } = inputProps;
+  const { field: formField } = useController({ name: id, defaultValue: false });
 
   return (
-    <FormControlLabel control={<Checkbox {...inputProps} />} label={label} />
+    <FormControlLabel
+      key={key}
+      {...props}
+      {...formField}
+      checked={formField.value}
+      control={<Checkbox />}
+      label={label}
+    />
   );
 };
--- packages/mui/src/components/SelectField.tsx
+++ packages/mui/src/components/SelectField.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
+import { useController } from "react-hook-form";
 import { AutoFormFieldProps } from "@autoform/react";
 
 export const SelectField: React.FC<AutoFormFieldProps> = ({
+  id,
   field,
   value,
   inputProps,
   error,
 }) => {
-  const { key, ...props } = inputProps;
+  const { key, onChange, onBlur, ref, ...props } = inputProps;
+  const { field: formField } = useController({ name: id, defaultValue:'' });
 
   return (
-    <Select error={!!error} fullWidth {...props} defaultValue={value}>
+    <Select error={!!error} fullWidth {...props} {...formField}>
       {(field.options || []).map(([key, label]) => (
         <MenuItem key={key} value={label}>
           {label}
