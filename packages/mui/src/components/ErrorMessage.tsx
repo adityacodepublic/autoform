d6ee7a6584f8e2ab7896c932f64dad7f29463943
gitcode <website0wordpress@gmail.com>
2025-03-16 12:35:18 +0530

    fix: ErrorMessage styles

--- packages/mui/src/components/ErrorMessage.tsx
+++ packages/mui/src/components/ErrorMessage.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import FormHelperText from "@mui/material/FormHelperText";
 
 export const ErrorMessage: React.FC<{ error: string }> = ({ error }) => (
-  <FormHelperText error>{error}</FormHelperText>
+  <FormHelperText error style={{ marginBottom: 8 }}>{error}</FormHelperText>
 );
