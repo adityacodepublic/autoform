55bff41276e453cb6c8aae93e1928a4b56019195
gitcode <website0wordpress@gmail.com>
2025-07-16 18:49:44 +0530

    chore: add form prop usage in mantine copmonent

--- apps/web/components/Mantine.tsx
+++ apps/web/components/Mantine.tsx
@@ -2,18 +2,24 @@ import { createTheme, MantineProvider } from "@mantine/core";
 import { AutoForm } from "@autoform/mantine";
 import { zodSchemaProvider } from "./utils";
 import "@mantine/core/styles.css";
+import { createForm } from "@autoform/react";
 
 function Mantine() {
   const theme = createTheme({});
+  const form = createForm();
   return (
     <MantineProvider theme={theme}>
       <AutoForm
+        form={form}
         schema={zodSchemaProvider}
         onSubmit={(data) => {
           console.log(JSON.stringify(data, null, 2));
         }}
         withSubmit
       />
+      <button onClick={() => form.setValue("username", "wow")}>
+        Set Username
+      </button>
     </MantineProvider>
   );
 }
